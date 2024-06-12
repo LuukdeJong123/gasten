1a1d5f2e413d1dc5dd19a8ffffafbe6adf488550
LuukdeJong123 <l-u-u-k@live.nl>
2024-06-12 16:24:08 +0100

    update test with dcgan-v2

--- src/optimization/gasten_gridsearch_step1.py
+++ src/optimization/gasten_gridsearch_step1.py
@@ -1,7 +1,6 @@
 import torch
 from torch.optim import Adam
 from sklearn.model_selection import ParameterSampler
-from tqdm import tqdm
 import argparse
 from dotenv import load_dotenv
 import wandb
@@ -104,7 +103,7 @@ def main():
         'd_beta1': [0.1, 0.5, 0.9],
         'g_beta2': [0.1, 0.5, 0.9],
         'd_beta2': [0.1, 0.5, 0.9],
-        'n_blocks': [8]
+        'n_blocks': [3,4,5]
     }
 
     # Training loop with grid search for hyperparameter optimization
